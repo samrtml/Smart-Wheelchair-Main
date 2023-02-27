662568532873a60e586d79003d6db3959bd5ad32
samrtml <115736130+samrtml@users.noreply.github.com>
2023-02-27 15:37:42 +0000

    Add files via upload

---
+++ RealSense/detection.py
@@ -0,0 +1,28 @@
+import rospy
+from sensor_msgs.msg import Image as msg_Image
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+import os
+
+class ImageListener:
+    def __init__(self, topic):
+        self.topic = topic
+        self.bridge = CvBridge()
+        self.sub = rospy.Subscriber(topic, msg_Image, self.imageDepthCallback)
+
+    def imageDepthCallback(self, data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
+            pix = (data.width/2, data.height/2)
+            sys.stdout.write('%s: Depth at center(%d, %d): %f(mm)\r' % (self.topic, pix[0], pix[1], cv_image[pix[1], pix[0]]))
+            sys.stdout.flush()
+        except CvBridgeError as e:
+            print(e)
+            return
+
+
+if __name__ == '__main__':
+    rospy.init_node("depth_image_processor")
+    topic = '/camera/depth/image_rect_raw'  # check the depth image topic in your Gazebo environmemt and replace this with your
+    listener = ImageListener(topic)
+    rospy.spin()
